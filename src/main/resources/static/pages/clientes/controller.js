f2f3b1d12bbccf1b73dbf518a6c8d552c7890c9e
IgnacioA9 <ignacio.arrieta.m@gmail.com>
2024-05-12 17:51:41 -0600

    Actualizacion de tablas
    Se actualizaron el formato y tamaño de las distintas tablas

--- src/main/resources/static/pages/clientes/controller.js
+++ src/main/resources/static/pages/clientes/controller.js
@@ -6,9 +6,29 @@ var state ={
     mode: "" // ADD, EDIT
 }
 
-document.addEventListener("DOMContentLoaded",loaded);
+document.addEventListener("DOMContentLoaded", loaded);
 
-async function loaded(event){
-    try{ await menu();} catch(error){return;}
+async function loaded(event) {
+    try {
+        await menu();
+    } catch (error) {
+        return;
+    }
+
+    // Obtener referencia al botón btnCreate y al popup
+    const btnCreate = document.getElementById("btnCreate");
+    const popup = document.querySelector(".popup");
+
+    // Agregar evento click al botón btnCreate
+    btnCreate.addEventListener("click", function() {
+        // Agregar la clase .active al popup
+        popup.classList.add("active");
+    });
+
+    // Función para cerrar el popup cuando se hace clic en el botón de cerrar
+    const closeBtn = document.querySelector(".close-btn");
+    closeBtn.addEventListener("click", function() {
+        popup.classList.remove("active");
+    });
     fetchAndList();
 }
